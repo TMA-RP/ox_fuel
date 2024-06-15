fad25eeb21363af5ef2843cd7865df7dd72e89ae
M3RY <themeryland@gmail.com>
2024-06-15 10:17:37 +0200

    our modif

--- config.lua
+++ config.lua
@@ -2,53 +2,53 @@ if not lib.checkDependency('ox_lib', '3.22.0', true) then return end
 if not lib.checkDependency('ox_inventory', '2.30.0', true) then return end
 
 return {
-	-- Get notified when a new version releases
-	versionCheck = true,
+    -- Get notified when a new version releases
+    versionCheck = true,
 
-	-- Enable support for ox_target
-	ox_target = true,
+    -- Enable support for ox_target
+    ox_target = true,
 
-	/*
+    /*
 	* Show or hide gas stations blips
 	* 0 - Hide all
 	* 1 - Show nearest (5000ms interval check)
 	* 2 - Show all
 	*/
-	showBlips = 0,
+    showBlips = 0,
 
-	-- Total duration (ex. 10% missing fuel): 10 / 0.25 * 250 = 10 seconds
+    -- Total duration (ex. 10% missing fuel): 10 / 0.25 * 250 = 10 seconds
 
-	-- Fuel refill value (every 250msec add 0.25%)
-	refillValue = 0.625,
+    -- Fuel refill value (every 250msec add 0.25%)
+    refillValue = 0.625,
 
-	-- Fuel tick time (every 250 msec)
-	refillTick = 250,
+    -- Fuel tick time (every 250 msec)
+    refillTick = 250,
 
-	-- Fuel cost (Added once every tick)
-	priceTick = 0.625,
+    -- Fuel cost (Added once every tick)
+    priceTick = 0.625,
 
-	-- Can durability loss per refillTick
-	durabilityTick = 1.3,
+    -- Can durability loss per refillTick
+    durabilityTick = 1.3,
 
-	-- Enables fuel can
-	petrolCan = {
-		enabled = true,
-		duration = 12000,
-		price = 50,
-		refillPrice = 30,
-	},
+    -- Enables fuel can
+    petrolCan = {
+        enabled = true,
+        duration = 12000,
+        price = 50,
+        refillPrice = 30,
+    },
 
-	---Modifies the fuel consumption rate of all vehicles - see [`SET_FUEL_CONSUMPTION_RATE_MULTIPLIER`](https://docs.fivem.net/natives/?_0x845F3E5C).
-	globalFuelConsumptionRate = 2.5,
+    ---Modifies the fuel consumption rate of all vehicles - see [`SET_FUEL_CONSUMPTION_RATE_MULTIPLIER`](https://docs.fivem.net/natives/?_0x845F3E5C).
+    globalFuelConsumptionRate = 4.5,
 
-	-- Gas pump models
-	pumpModels = {
-		`prop_gas_pump_old2`,
-		`prop_gas_pump_1a`,
-		`prop_vintage_pump`,
-		`prop_gas_pump_old3`,
-		`prop_gas_pump_1c`,
-		`prop_gas_pump_1b`,
-		`prop_gas_pump_1d`,
-	}
+    -- Gas pump models
+    pumpModels = {
+        `prop_gas_pump_old2`,
+        `prop_gas_pump_1a`,
+        `prop_vintage_pump`,
+        `prop_gas_pump_old3`,
+        `prop_gas_pump_1c`,
+        `prop_gas_pump_1b`,
+        `prop_gas_pump_1d`,
+    }
 }
